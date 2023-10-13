1555649ddefd51691371d95969bcf252992571ed
SwarabRaul <swarab.raul@gmail.com>
2023-10-13 13:08:23 +0530

    feat(app.js & app.css): added App.js & App.css for a Dummy User Table

---
+++ src/UserTable.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import "./UserTable.css";
+
+const UserTable = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Fetch data from the API
+        fetch("https://dummyjson.com/users")
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data.users);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Dummy User Table</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Username</th>
+                        <th>Domain</th>
+                        <th>University</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {loading ? (
+                        <tr>
+                            <td colSpan="3">Loading...</td>
+                        </tr>
+                    ) : (
+                        users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.firstName} {user.lastName}</td>
+                                <td>{user.email}</td>
+                                <td>{user.gender}</td>
+                                <td>{user.username}</td>
+                                <td>{user.domain}</td>
+                                <td>{user.university}</td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </table>
+            <button onClick={() => window.location.href = "https://www.google.com"}>Back to Google</button>
+        </div>
+    );
+};
+
+export default UserTable;
